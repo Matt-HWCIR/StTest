29d60beaaa5ab883826e85afe90365aad1701acb
matt-hwcir <matthew.ebert@imail.org>
2012-01-10 20:17:32 +0000

    log

--- public/scripts/DrawPad.js
+++ public/scripts/DrawPad.js
@@ -36,6 +36,7 @@ define(['data','DrawUtils','LineShape','SymptomShape','GroupShape','SelectorTool
 
 
 	module.initChime=function(){
+        console.log('chime initialized');
 		chime=new Audio("sounds/button-17.mp3");
 		chime.load();
 	};
